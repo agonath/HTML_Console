249092e81864c3e029ef1c77633da11d950d1371
Agonath <mirko.braun.frankfurt@gmx.de>
2020-04-24 16:13:49 +0200

    Added selection of text functionality. But this is not finished yet. I also tested serveral stuff here and there. See comments.

--- JS/console.js
+++ JS/console.js
@@ -29,18 +29,30 @@ class MyConsole extends Object
 		this.lastTimeStamp = performance.now();
 		this.updateReqFlag = false; // is an update required?
 
+		// TODO: not yet finished -- Selection
+		this.shiftKeyPressed = false;// Shift key is pressed? Needed for the arrow key handling functions.
+		this.selectionStartPos = -1; // where the selection via shift + arrow key starts (cursor position)
+		this.selectionEndPos = -1;   // where the selection via shift + arrow key ends (cursor position)
+		this.selectionActive = false; // Indicates if there is currently a selected text active.
+		this.selectedPositions = []; // Array of cursor positions (numbers) wich have been selected via shift key and arrow left/right
+
+		//Node for selected chars, created once only.
+		this.selectionNode = document.createElement("span");
+		this.selectionNode.setAttribute("class", "selected-text");
+		this.selectionNode.setAttribute("id", "selected-text");
+
 
 		console.log("Console Version: " + VERSION);
 
 
-		//Cursor-Node nur einmal erzeugen
+		//The cursor node is created once only. / Cursor-Node nur einmal erzeugen.
 		this.curNode = document.createElement("span");
 		this.curNode.setAttribute("class", "cursor");
 		this.curNode.setAttribute("id", "cursor");
 
 
-		//Test-Object
-		this.handlerArray = new Array(256);
+		//Test of new input handler, but array lookup is a lot slower than the normal "switch/case" statement.
+		//this.handlerArray = new Array(256);
 		
 	}
 
@@ -57,14 +69,17 @@ class MyConsole extends Object
 			// Register input handler
 			const self = this; // needed to keep the reference of this to our console class object
 			_element.addEventListener("keydown", function(_e){ return self.handleInput(_e); } );
+			//_element.addEventListener("message", function(_e){ return self.handleInput(_e); } ); --> second fastest method
+			_element.addEventListener("keyup", function(_e){ return self.handleInput(_e); });
 
 			// Register update function
 			this.setIntervalId = setInterval(this.eventLoop.bind(this), FPS);
+			//window.requestAnimationFrame(this.eventLoop2.bind(this)); --> dont use this!!
 			
 			//Refresh buffer and make cursor visible
 			this._clearConsoleLineBuffer();
 
-			// Test for new input handler
+			// Test for new input handler, but seems a lot slower than the normal "switch/case" statement.
 			/*for(let i=0; i<256; i++)
 			{
 				this.handlerArray[i] = (_e, _textNode) => { return this._handleChars(_e, _textNode);}; // default, handle every input as normal char
@@ -90,7 +105,7 @@ class MyConsole extends Object
 
 
 	//
-	//	Event loop handler, all update goes here...
+	//	Event loop handler, all update goes here...compared to "window.requestAnimationFrame" ultra fast!!!
 	//
 	eventLoop()
 	{
@@ -100,16 +115,46 @@ class MyConsole extends Object
 		// only update if needed
 		if(t > FPS && this.updateReqFlag === true)
 		{
-			console.log("Eventloop...");
-			console.log("TextNode: " + this.textNode);
-			console.log("Console Buffer: " + this.consoleBuffer);
-			console.log("Cursor Position: " + this.cursorPosition);
+			// ---|DEBUG|---
+			//console.log("Eventloop1...");
+			//console.log("TextNode: " + this.textNode);
+			//console.log("Console Buffer: " + this.consoleBuffer);
+			//console.log("Cursor Position: " + this.cursorPosition);
 			this.updateCursorLine(this.textNode, this.consoleBuffer, this.cursorPosition);
 		}
 
 		this.lastTimeStamp = timeStamp;		
 	}
 
+
+	// Test new event via "window.requestAnimationFrame" loop - This is stupid slow extrem slow!!!!
+	/*eventLoop2(_time)
+	{
+		//console.log("start eventloop 2, Zeit in Millisekunden: " + _time);
+
+		//let timeStamp = performance.now();
+		//let t = timeStamp - this.lastTimeStamp;
+		let t = _time - this.lastTimeStamp;
+
+		// only update if needed
+		//if(t > FPS)// && this.updateReqFlag === true)
+
+		if(t > FPS && this.updateReqFlag === true)
+		{
+			console.log("Zeit > FPS (" + FPS + "): " + (t>FPS) + " Wert: " + t);
+
+			this.updateCursorLine(this.textNode, this.consoleBuffer, this.cursorPosition);
+
+			//window.requestAnimationFrame(this.eventLoop2.bind(this));
+		}
+
+		//this.lastTimeStamp = timeStamp;
+		this.lastTimeStamp = _time;
+
+
+		window.requestAnimationFrame(this.eventLoop2.bind(this));
+	}
+*/
 
 	//
 	// Updates the cursor position and input line.
@@ -160,6 +205,181 @@ class MyConsole extends Object
 	}
 
 
+
+	// TODO: Not finished, cursor position == selection start is missing.
+	updateCursorLine2(_textNode, _buffer, _cursorPosition)
+	{
+		const lenBuf = _buffer.length;
+		//console.log("Länge des Buffer: " + lenBuf);
+		const offsetEnd = lenBuf - _cursorPosition;
+		//console.log("Offset: " + offsetEnd);
+
+		switch(offsetEnd)
+		{
+			case 0: //Cursor is at the end
+			{
+				//check if there is an text selection active
+				switch(this.selectionActive)
+				{
+					case true: // color up the text :-)
+					{
+						// TODO: Check this!!!
+						//
+						// Cursor is at the end, means selection start position mut be smaller than the cursor position.
+						// The selection is made by using the right arrow key.
+						// And the cursor is always at the end. (see first switch condition)
+						let text = _buffer.slice(0, this.selectionStartPos); //the normal, not selected text
+						let selection = _buffer.slice(this.selectionStartPos, this.cursorPosition); // the selected text
+
+						//Update the content of the input line
+						// first the normal text
+						this.textNode.innerHTML = text;
+						// now add the <span> element node
+						this.textNode.append(this.selectionNode);
+						// update the content of this node with the currently selected text
+						this.selectionNode.innerHTML = selection;
+
+
+						//finally the cursor node
+						this.textNode.appendChild(this.curNode);
+						this.curNode.innerHTML = "&nbsp"; // --> Space else Cursor would not be visible at the end of line
+						break;
+					}
+
+					default: // standard behaviour, no text selection
+					{
+						this.textNode.innerHTML = _buffer;
+						this.textNode.appendChild(this.curNode);
+						this.curNode.innerHTML = "&nbsp"; // --> Space else Cursor would not be visible at the end of line
+						break;
+					}
+				}// switch(this.selectionActive)
+				break;
+			}
+
+			default:
+			{
+				//check if there is an text selection active
+				switch(this.selectionActive)
+				{
+					case true: // color up the text :-)
+					{
+						// TODO
+						
+						//-----------------------------
+						// Collect the text elements
+						//-----------------------------
+
+						// The cursor is somewhere at the line. (see first switch condition) Every selection is made by using the left arrow key.
+						if(this.cursorPosition < this.selectionStartPos)
+						{
+							// The selected text is always behind the cursor position. (right side of the cursor)
+						
+							//First get the normal text in front of the cursor.
+							const normalText = _buffer.slice(0, this.cursorPosition);
+
+							// Get the char covered by the cursor. Overlapping with the first selected char here.
+							const coveredChar = _buffer.slice(this.cursorPosition, this.cursorPosition+1);
+
+							//Now get the selected text, the first char is also covered by the cursor. So skip this one, else it would appear twice on screen.
+							//+1 to the end, because end of slicing is excluding the end value.
+							// If the selection begins somewhere at the line, then "cursor position = start position of selection".
+							const selectedText = _buffer.slice(this.cursorPosition+1, this.selectionStartPos+1); 
+
+							//Now get the text after the current selection.
+							const afterCurAndSelection = _buffer.slice(this.selectionStartPos+1, lenBuf); // TODO: seems to start one char to soon
+						
+						
+							//-----------------------------
+							// Now build the input line
+							//-----------------------------
+
+							// First the text in front of the cursor.
+							this.textNode.innerHTML = normalText;
+							// Second the cursor node
+							this.textNode.appendChild(this.curNode);
+							// Set up the content covered by the cursor
+							this.curNode.innerHTML = coveredChar;
+							// Add the selection node after the cursor node.
+							this.textNode.appendChild(this.selectionNode);
+							// Set up the content of the selection node.
+							this.selectionNode.innerHTML = selectedText;
+							// Now add the rest of the normal text to the end.
+							this.selectionNode.insertAdjacentHTML('afterend', afterCurAndSelection);
+						}
+						else
+						{
+							// The selected text is always in front of the cursor position. (left side of the cursor)
+							//First get the normal text in front of the selected text.
+							const normalText = _buffer.slice(0, this.selectionStartPos);
+
+							//Now get the selected text, the last char is also covered by the cursor. So skip this one, else it would appear twice on screen.
+							const selectedText = _buffer.slice(this.selectionStartPos, this.cursorPosition-1);
+
+							// Get the char covered by the cursor. Overlapping with the first selected char here.
+							const coveredChar = _buffer.slice(this.cursorPosition, this.cursorPosition+1);
+
+							//Now get the text after the cursor.
+							const afterCurAndSelection = _buffer.slice(this.cursorPosition+1, lenBuf);
+						
+						
+							//-----------------------------
+							// Now build the input line
+							//-----------------------------
+
+							// First the text in front of the cursor.
+							this.textNode.innerHTML = normalText;
+							// Second add the selection node after the normal text.
+							this.textNode.appendChild(this.selectionNode);
+							// Set up the content of the selection node.
+							this.selectionNode.innerHTML = selectedText;
+							// Now the cursor node
+							this.textNode.appendChild(this.curNode);
+							// Set up the content covered by the cursor
+							this.curNode.innerHTML = coveredChar;
+							// Now add the rest of the normal text to the end.
+							this.curNode.insertAdjacentHTML('afterend', afterCurAndSelection);
+						}
+
+						break;
+					}
+
+					default: // standard behaviour
+					{
+						//-----------------------------
+						// Collect the text elements
+						//-----------------------------
+
+						const beforeCur = _buffer.slice(0, _cursorPosition);
+						const coveredChar = _buffer.slice(_cursorPosition, _cursorPosition+1);
+						const afterCur = _buffer.slice(_cursorPosition+1, lenBuf);
+
+						//---|DEBUG|---
+						console.log("Vor Cursor: " + beforeCur);
+						console.log("Nach Cursor: " + afterCur);
+						
+						
+						//-----------------------------
+						// Now build the input line
+						//-----------------------------
+						
+						this.textNode.innerHTML = beforeCur;
+						this.textNode.appendChild(this.curNode);
+						this.curNode.innerHTML = coveredChar;
+						this.curNode.insertAdjacentHTML('afterend', afterCur);
+						break;
+					}
+				}//switch(this.selectionActive)
+				break;
+			}
+
+		}//switch(offsetEnd)
+
+		this.updateReqFlag = false;
+		return this.textNode.innerHTML;
+	}
+
+
 	//
 	// Update Buffer, preserving the current buffer content!
 	//
@@ -179,6 +399,7 @@ class MyConsole extends Object
 		}
 
 		this.updateReqFlag = true;
+		//window.postMessage("update");
 		return this.consoleBuffer;
 	}
 
@@ -256,16 +477,66 @@ class MyConsole extends Object
 	handleInput(e)
 	{
 		e.preventDefault();
+		
+		// ---|Debug|---
+		//console.log(e.type);
 
 		switch(e.type)
 		{
+			case 'message': // TODO: Plugins via worker threads need to update the console buffer this way.
+			{
+				switch(e.data) // TODO: Check source of message first!
+				{
+					case "update":
+					{
+						//console.log("Meldung erhalten: " + e.data);
+						this.updateCursorLine(this.textNode, this.consoleBuffer, this.cursorPosition);
+						break;
+					}
+				}
+				break;
+			}
+
+			case 'keyup':
+			{
+				switch(e.keyCode)
+				{
+					case 16: //Shift-key
+					{
+						// update key state
+						this.shiftKeyPressed = e.shiftKey;
+
+						switch(this.selectionActive)
+						{
+							case true:
+							{
+								//there is an active selection, save the end position wich equals to the current cursor position
+								this.selectionEndPos = this.cursorPosition;
+								break;
+							}
+
+							default: // No selection active, nothing needs to be done here.
+							{ break; }
+						}// switch(this.selectionActive)
+
+						break;
+					}
+					
+					default: // No more keys handled here...
+					{ break; }
+
+				} // switch(e.keyCode)
+
+				break; //keyup
+			}
+
+
 			case 'keydown':
-			case 'keypressed':
 			{
 				e.stopPropagation();
 				
 				//---|Debug|---
-				//console.log(e);
+				console.log(e);
 				//console.log("Länge: " + e.key.length);
 
 				switch(e.keyCode)
@@ -294,20 +565,24 @@ class MyConsole extends Object
 						break;
 					}
 
-					case 37: // Arrow Left
+					case 37: // Arrow Left + check fpr shift key
 					{
+						this.shiftKeyPressed = e.shiftKey;
+						console.log("Shiftkey: " + this.shiftKeyPressed);
 						this._handleLeftArrowKey(e, this.textNode);
 						break;
 					}
 
 					case 38: // Arrow Up
 					{
-						_handleArrowKeysDummy(e, this.textNode);
+						this._handleArrowKeysDummy(e, this.textNode);
 						break;
 					}
 					
-					case 39: // Arrow Right
+					case 39: // Arrow Right + check fpr shift key
 					{
+						this.shiftKeyPressed = e.shiftKey;
+						console.log("Shiftkey: " + this.shiftKeyPressed);
 						this._handleRightArrowKey(e, this.textNode);
 						break;
 					}
@@ -326,7 +601,7 @@ class MyConsole extends Object
 				
 					default:
 					{
-						//console.log("Event: " + e.keyCode);
+						console.log("Event: " + e.keyCode);
 						this._handleChars(e, this.textNode);
 						break;
 					}	
@@ -348,7 +623,7 @@ class MyConsole extends Object
 				}// switch keycode
 			}
 
-			default: // No key pressed...
+			default: // Nothing...
 			{ break; }
 		}
 	}// end handle input
@@ -359,7 +634,7 @@ class MyConsole extends Object
 	// 
 	// Not used.
 	//
-	handleInput2(e)
+	/*handleInput2(e)
 	{
 		e.preventDefault();
 
@@ -383,7 +658,7 @@ class MyConsole extends Object
 			{ break; }
 		}
 	}// end handle input
-
+*/
 
 
 	//
@@ -395,6 +670,9 @@ class MyConsole extends Object
 		{
 			case 1: // no special keys, every printable keys length equals to 1 is accepted (stupid, but working)
 			{
+				// cancel an active selection of text
+				this.selectionActive = false;
+
 				console.log(String.fromCharCode(_e.keyCode));
 				let t = this.updateBuffer(_e.key, this.cursorPosition);
 				console.log("Updated buffer: " + t);
@@ -414,6 +692,10 @@ class MyConsole extends Object
 		this.curNode.innerHTML = "";
 		this.consoleBuffer = "";
 		this.cursorPosition = 0;
+
+		// cancel an active selection of text
+		this.selectionActive = false;
+
 		// direct update the prevent ghost images
 		this.updateCursorLine(this.textNode, this.consoleBuffer, this.cursorPosition);
 	}
@@ -438,7 +720,7 @@ class MyConsole extends Object
 					// Print the line to console and update the line counter accordingly.
 					this.printLine(beforeCur);
 	
-					//Clear the input line buffer
+					//Clear the input line buffer and cancle an active text selection
 					this._clearConsoleLineBuffer();
 
 					console.log(String.fromCharCode(_e.keyCode));
@@ -456,8 +738,13 @@ class MyConsole extends Object
 	{
 		this._removeCharsFromBuffer();
 		console.log("Textzeile nach Backspace: " + this.consoleBuffer);
+		
+		// cancel an active selection of text
+		this.selectionActive = false;
+
 		// Update
 		this.updateReqFlag = true;
+		//window.postMessage("update");
 
 		console.log("Backspace Key Code: " + String.fromCharCode(_e.keyCode));
 	}
@@ -470,8 +757,13 @@ class MyConsole extends Object
 	{
 		this._removeCharsFromBuffer(1, true);
 		console.log("Textzeile nach Entf: " + this.consoleBuffer);
+
+		// cancel an active selection of text
+		this.selectionActive = false;
+
 		// Update
 		this.updateReqFlag = true;
+		//window.postMessage("update");
 	}
 
 
@@ -485,9 +777,57 @@ class MyConsole extends Object
 		// Handle left arrow key, should move the cursor to the left
 		if(this.cursorPosition > 0)
 		{
-			console.log("Cursor position: " + this.cursorPosition);
+			//update cursor position
 			this.cursorPosition -= 1;
 			this.updateReqFlag = true;
+			//window.postMessage("update");
+		}
+	}
+
+
+	_handleLeftArrowKey2(_e, _textNode)
+	{	
+		// Handle left arrow key, should move the cursor to the left
+		if(this.cursorPosition > 0)
+		{
+			console.log("Cursor position: " + this.cursorPosition);
+
+			switch(this.shiftKeyPressed)
+			{
+				case true:
+				{
+					// Check if we need to set the selection flag
+					switch(this.selectionActive)
+					{
+						case false:
+						{
+							// No selection is currently active, activate it
+							this.selectionActive = true;
+							// save the start position of the selection
+							this.selectionStartPos = this.cursorPosition;
+						}
+
+						default:
+						{ break; } // Selection already active, nothing more needs to be done here.
+
+					}// switch(this.selectionActive)
+
+					break;
+				}// case true
+
+				default:
+				{
+					// arrow key pressed without shift key, cancle an active text selection
+					this.selectionActive = false; 
+					break; 
+				}
+
+			} // switch(this.shiftKeyPressed)
+
+			// finally update cursor position
+			this.cursorPosition -= 1;
+			this.updateReqFlag = true;
+			//window.postMessage("update");
 		}
 	}
 
@@ -503,6 +843,52 @@ class MyConsole extends Object
 			console.log("Cursor position: " + this.cursorPosition);
 			this.cursorPosition += 1;
 			this.updateReqFlag = true;
+			//window.postMessage("update");
+		} 
+	}
+
+	_handleRightArrowKey2(_e, _textNode)
+	{
+		// Handle right arrow key, should move the cursor to the right
+		if(this.cursorPosition < this.consoleBuffer.length)
+		{
+			console.log("Cursor position: " + this.cursorPosition);
+
+			switch(this.shiftKeyPressed)
+			{
+				case true:
+				{
+					// Check if we need to set the selection flag
+					switch(this.selectionActive)
+					{
+						case false:
+						{
+							// No selection is currently active, activate it
+							this.selectionActive = true;
+							// save the start position of the selection
+							this.selectionStartPos = this.cursorPosition;
+						}
+
+						default:
+						{ break; } // Selection already active, nothing more needs to be done here.
+
+					}// switch(this.selectionActive)
+
+					break;
+				}// case true
+
+				default:
+				{
+					// arrow key pressed without shift key, cancle an active text selection
+					this.selectionActive = false; 
+					break; 
+				}
+
+			} // switch(this.shiftKeyPressed)
+
+			this.cursorPosition += 1;
+			this.updateReqFlag = true;
+			//window.postMessage("update");
 		} 
 	}
 
@@ -519,8 +905,12 @@ class MyConsole extends Object
 	 //
 	 _handlePos1Key(_e, _textNode)
 	 {
+		 // cancel an active selection of text
+		 this.selectionActive = false;
+		 
 		 this.cursorPosition = 0;
 		 this.updateReqFlag = true;
+		 //window.postMessage("update");
 	 }
 
 
@@ -530,8 +920,12 @@ class MyConsole extends Object
 	 //
 	 _handleEndKey(_e, _textNode)
 	 {
+		 // cancel an active selection of text
+		 this.selectionActive = false;
+		 
 		 this.cursorPosition = this.consoleBuffer.length;
 		 this.updateReqFlag = true;
+		 //window.postMessage("update");
 	 }
 	
 	
