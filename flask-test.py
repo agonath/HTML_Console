74e5596cc94266a2d1e406046823a40464e32a28
Agonath <mirko.braun.frankfurt@gmx.de>
2022-10-30 21:42:08 +0100

    Kleinere Änderung im Backend, nocht nicht fertig.

--- flask-test.py
+++ flask-test.py
@@ -9,52 +9,34 @@ saveOut = sys.stdout
 saveIn = sys.stdin
 saveError = sys.stderr
 
-# set std streams, defaults to system values
-sys.stdout = _stdout
-sys.stdin = _stdin
-sys.stderr = _stderror
-
 SECRET_KEY = "Super Duper Secret Key" #TODO: Change this, secret key to sign Flask's sessions
 RANDOM_KEY = "not Random" #TODO: Change this
+
+connected : bool = False
+
 
 flaskApp = Flask(__name__);
 
 @flaskApp.route("/", methods=['GET', 'POST'])
 def index():
-    return app.index(request); 
+    if(request.method == 'POST'):
+        return execute(request.json); # Todo asynchron machen
+    else:
+        return render_template('index.html') 
 
+
+def execute(param:str) -> str:
+     try:
+        #Todo
+        sys.stdin.write(param);
+        return str(sys.stdout);
+
+     except(Exception):
+        return str(sys.stderr)
     
-
-class MyApp(object):
-
-    __slots__ = ["_connected"]
-
-    def __init__(self):
-        self._connected = False
-    
-
-    def index(self, request):
-
-        if(request.method == 'POST' and self._connected == True):
-            return self.execute(request.json); # Todo asynchron machen
-        else:
-            self._connected = True;
-            return render_template('index.html')
-
-    
-    """
-        Execute something
-        Parameter: String --> what to execute
-    """
-    def execute(self, String:_param):
-        try:
-            #Todo
-        except(e):
-            return sys.stderr
 
 
 if __name__ == "__main__":
     #sys.argv --> Parameter
-    app = MyApp();
     flaskApp.config.from_object(__name__)
     flaskApp.run(host="127.0.0.1", port="5000")
