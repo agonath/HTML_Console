5532c9ceb8811b25f75f3d5ae7ac69bf9f5b4da6
Agonath <mirko.braun.frankfurt@gmx.de>
2024-09-15 12:42:59 +0200

    add url check to loader class

--- src/JS/loader.ts
+++ src/JS/loader.ts
@@ -1,28 +1,54 @@
 "use strict";
 
 // Enum for possible messages.
-const enum MESSAGES { NONE=0, UPDATE=1, CLS=2, SEND=3, RECEIVE=4 };
+export const enum MESSAGES { NONE=0, UPDATE=1, CLS=2, SEND=3, RECEIVE=4 };
+
+// Message Interface
+export interface DATA_PCK {
+    type: MESSAGES;
+    data: any;
+    status: number;
+}
 
 //
 // TODO class for receive and send messages to server
 // This version is just for demo....
 // 
 
-class Loader extends Object
+export class Loader extends Object
 {
     private result :string;
-    private allowedServers :string[];
+    private allowedServers :URL[];
 
-    constructor(_serverAddr=["127.0.0.1:80"])
+    constructor(_serverAddrArray :string[]=["127.0.0.1:80"])
     {
         super();
         
         this.result="";
-        this.allowedServers = _serverAddr;
 
-        // Debug
-        for(var x in this.allowedServers)
-        {   console.log(this.allowedServers[x]); }
+        for(let x in _serverAddrArray.keys)
+        {   
+            if(URL.canParse(_serverAddrArray[x]))
+            {
+                this.allowedServers.push(URL.parse(_serverAddrArray[x]));
+                // Debug
+                console.log(`Add server adress: ${this.allowedServers[x]}`);
+            } 
+        }
+    }
+
+
+    /*
+    * Helper, check if URL is in our allowed URLs list.
+    */
+    isURLAllowed(_url :string="")
+    {
+        for(let entry in this.allowedServers)
+            {
+                if( this.allowedServers[entry].host === _url)
+                { return true; }
+            }
+        return false;
     }
 
 
@@ -45,30 +71,44 @@ class Loader extends Object
     //
     //
     //
-    public async sendData(_server, _data, _method :string ='POST')
+    public async sendData(_server :URL, _data :any, _method :string ='POST')
     {
-        let payload :RequestInit = {
+        // Check URL first
+        if(true === this.isURLAllowed(_server.host))
+        {
+            let payload :RequestInit = {
                         method: _method,
                         cache: 'no-cache',
                         headers: {'Content-Type':'application/json;charset=utf-8'},
                         body: JSON.stringify(_data)
-                      }
+            }
 
-        let response = await fetch(_server, payload);
+            let response = await fetch(_server, payload);
 
-        console.log("ReceiveData -> response.status: " + response.status);
-        this.receiveData(await response.json(), response.type, response.status);
+            console.log("ReceiveData -> response.status: " + response.status);
+            this.receiveData(await response.json(), response.type, response.status, response.url);
+        }
     }
 
 
     // TODO
     // Send results from server to console.
     //
-    public async receiveData(_data, _type :ResponseType, _status :number)
+    public async receiveData(_data, _type :ResponseType, _status :number, _url:string)
     {
-        let msg = {type:MESSAGES.RECEIVE, data:_data, status:_status};
-        window.postMessage(msg); // TODO check origin
-        console.log("In sendResultTo Console mit Nachricht: " + msg + " und Daten: " + JSON.stringify(_data));
+        // Check URL first
+        if(true === this.isURLAllowed(_url))
+        {
+            const msg :DATA_PCK = {   
+                                    type:MESSAGES.RECEIVE, 
+                                    data:_data, 
+                                    status:_status
+                                };
+
+            window.postMessage(msg, _url);
+
+            console.log(`In sendResultTo Console von URL: ${_url} mit Nachricht: ${msg} und Daten: ${JSON.stringify(_data)}`);
+        }
     }
 
     // Event handler -- not used at the moment
@@ -79,7 +119,7 @@ class Loader extends Object
             case 'message': //handle messages only -- 
             {
                 let allowedOrigin = false;
-                for(var x in this.allowedServers)
+                for(let x in this.allowedServers)
                 {
                     if(_event.origin.toLowerCase() === this.allowedServers[x])
                     {   allowedOrigin = true; }
